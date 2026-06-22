7f7c4b3442825c332cb56fa784de6ec833bc566b
Joshua Bryant <jbryant81@gatech.edu>
2026-06-22 15:00:33 -0500

    Added references to enumerations file and list of attacks.

--- cards.py
+++ cards.py
@@ -4,42 +4,7 @@ from typing import Any, Callable, Dict, Optional, List
 from enum import Enum
 
 from attacks import Attack
-
-class CardType(Enum):
-    POKEMON = "Pokemon"
-    TRAINER = "Trainer"
-    ENERGY  = "Energy"
-
-class CardSubType(Enum):
-    BASIC_POKEMON =     "Basic Pokemon"
-    STAGE1_POKEMON =    "Stage 1 Pokemon"
-    STAGE2_POKEMON =    "Stage 2 Pokemon"
-    EVOLVED_POKEMON =   "Evolved Pokemon"
-    EVOLUTION_POKEMON = "Evolution Pokemon"
-    EX_POKEMON =        "ex Pokemon"
-    TERA_POKEMON =      "Tera Pokemon"
-    RULEBOX_POKEMON =   "Rulebox Pokemon"
-    ITEM_TRAINER =      "Item Trainer"
-    TOOL_TRAINER =      "Tool Trainer"
-    SUPPORTER_TRAINER = "Supporter Trainer"
-    STADIUM_TRAINER   = "Stadium Trainer"
-    BASIC_ENERGY =      "Basic Energy"
-    SPECIAL_ENERGY =    "Special Energy"
-    ACE_SPEC =          "Ace Spec"
-
-class EnergyType(ENUM):
-    GRASS     = "Grass"
-    FIRE      = "Fire"
-    WATER     = "Water"
-    LIGHTNING = "Lightning"
-    PSYCHIC   = "Psychic"
-    FIGHTING  = "Fighting"
-    DARKNESS  = "Darkness"
-    METAL     = "Metal"
-    FAIRY     = "Fairy"
-    DRAGON    = "Dragon"
-    COLORLESS = "Colorless"
-    RAINBOW   = "Rainbow"
+from enumerations import CardType, CardSubType, EnergyType
 
 @dataclass
 class Card:
